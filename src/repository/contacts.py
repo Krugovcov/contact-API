11f24f4900c42807c92cc00493805562a257355f
Matviy <krugovcovmatvey@gmail.com>
2025-06-17 11:00:27 +0300

    Enhance contact retrieval functions and add unit tests for repository methods

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -2,14 +2,26 @@ from datetime import date, datetime, timedelta
 from sqlalchemy import select, or_
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.entity.models import ContactBook
+from src.entity.models import ContactBook, User
 from src.schemas.contact import ContactBookSchema, ContactBookSchemaUpdateSchema
 
 
 
-async def get_contacts(limit: int, offset: int, db: AsyncSession, name: str = None, secondname: str = None, email: str = None, user_id: int = None):
-    stmt = select(ContactBook).offset(offset).limit(limit)
-    
+async def get_contacts(limit: int, offset: int, db: AsyncSession, user: User, name: str = None, secondname: str = None, email: str = None, user_id: int = None):
+    """
+    Retrieve a list of contacts from the database with optional filtering and pagination.
+    Args:
+        limit (int): The maximum number of contacts to return.
+        offset (int): The number of contacts to skip before starting to collect the result set.
+        db (AsyncSession): The asynchronous database session to use for the query.
+        name (str, optional): Filter contacts by name (case-insensitive, partial match).
+        secondname (str, optional): Filter contacts by second name (case-insensitive, partial match).
+        email (str, optional): Filter contacts by email (case-insensitive, partial match).
+        user_id (int, optional): Filter contacts by the associated user ID.
+    Returns:
+        List[ContactBook]: A list of contacts matching the specified filters and pagination.
+    """
+    stmt = select(ContactBook).filter(ContactBook.user_id == user.id)
     if user_id is not None:
         stmt = stmt.filter(ContactBook.user_id == user_id)
     if name:
@@ -18,31 +30,31 @@ async def get_contacts(limit: int, offset: int, db: AsyncSession, name: str = No
         stmt = stmt.filter(ContactBook.secondname.ilike(f'%{secondname}%'))
     if email:
         stmt = stmt.filter(ContactBook.email.ilike(f'%{email}%'))
-        
+    stmt = stmt.offset(offset).limit(limit)
     result = await db.execute(stmt)
     return result.scalars().all()
 
 
 async def get_contacts_birthday(db: AsyncSession, user_id: int):
     """
-    Short summary of the function.
-
+    Retrieve contacts from the database whose birthdays fall within the next 7 days for a specific user.
     Args:
-        db (AsyncSession): Database session.
-        user_id (int): ID of the user.
-
+        db (AsyncSession): The asynchronous database session.
+        user_id (int): The ID of the user whose contacts are being queried.
     Returns:
-        ReturnType: Description of the return value.
+        List[ContactBook]: A list of ContactBook objects representing contacts with upcoming birthdays.
+    Notes:
+        - Handles the case where the 7-day period crosses over the end of the year (December to January).
+        - Assumes that the ContactBook model has a 'born_day' attribute of type date.
     """
+    
     today = datetime.today().date()
     seven_days_later = today + timedelta(days=7)
     
     stmt = select(ContactBook).filter(
         ContactBook.user_id == user_id,
         or_(
-            # Коли період не переходить через новий рік
             (ContactBook.born_day >= today) & (ContactBook.born_day <= seven_days_later),
-            # Коли період переходить через новий рік (декабрь-січень)
             (ContactBook.born_day >= date(today.year, 1, 1)) & 
             (ContactBook.born_day <= date(today.year, 1, 7)) & 
             (today.month == 12)
@@ -52,12 +64,35 @@ async def get_contacts_birthday(db: AsyncSession, user_id: int):
     return result.scalars().all()
 
 async def get_contact_by_id(contact_id: int, db: AsyncSession, user_id: int):
+    """
+    Retrieve a contact by its ID for a specific user.
+    Args:
+        contact_id (int): The unique identifier of the contact to retrieve.
+        db (AsyncSession): The asynchronous database session to use for the query.
+        user_id (int): The ID of the user who owns the contact.
+    Returns:
+        ContactBook or None: The contact object if found, otherwise None.
+    """
     
     stmt = select(ContactBook).filter_by(id=contact_id, user_id=user_id)
     result = await db.execute(stmt)
     return result.scalar_one_or_none()
 
 async def create_contact(body: ContactBookSchema, db: AsyncSession, user_id: int):
+    """
+    Asynchronously creates a new contact record in the database for a specified user.
+
+    Args:
+        body (ContactBookSchema): The schema object containing contact details to be created.
+        db (AsyncSession): The asynchronous database session used for database operations.
+        user_id (int): The ID of the user to whom the contact will be associated.
+
+    Returns:
+        ContactBook: The newly created contact instance with updated fields from the database.
+
+    Raises:
+        SQLAlchemyError: If there is an error during the database transaction.
+    """
     contact_data = body.model_dump(exclude_unset=True)
     contact_data['user_id'] = user_id  
     contact = ContactBook(**contact_data)
@@ -68,6 +103,18 @@ async def create_contact(body: ContactBookSchema, db: AsyncSession, user_id: int
 
 
 async def update_contact(contact_id: int, body: ContactBookSchemaUpdateSchema, db: AsyncSession, user_id: int):
+    """
+    Asynchronously updates an existing contact in the database for a specific user.
+
+    Args:
+        contact_id (int): The unique identifier of the contact to update.
+        body (ContactBookSchemaUpdateSchema): The schema containing the fields to update.
+        db (AsyncSession): The asynchronous database session.
+        user_id (int): The unique identifier of the user who owns the contact.
+
+    Returns:
+        ContactBook or None: The updated contact object if found and updated, otherwise None.
+    """
     stmt = select(ContactBook).filter_by(id=contact_id, user_id=user_id)
     result = await db.execute(stmt)
     contact = result.scalar_one_or_none()
@@ -82,6 +129,17 @@ async def update_contact(contact_id: int, body: ContactBookSchemaUpdateSchema, d
 
 
 async def delete_contact(contact_id: int, db: AsyncSession, user_id: int):
+    """
+    Asynchronously deletes a contact from the database for a specific user.
+
+    Args:
+        contact_id (int): The ID of the contact to be deleted.
+        db (AsyncSession): The asynchronous database session.
+        user_id (int): The ID of the user who owns the contact.
+
+    Returns:
+        ContactBook or None: The deleted contact object if found and deleted, otherwise None.
+    """
     stmt = select(ContactBook).filter_by(id=contact_id, user_id=user_id)
     result = await db.execute(stmt)
     contact = result.scalar_one_or_none()
---
+++ tests/test_unit_repository_contacts.py
@@ -0,0 +1,137 @@
+from datetime import date
+import unittest
+from unittest.mock import AsyncMock, MagicMock
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.entity.models import User, ContactBook
+from src.schemas.contact import ContactBookSchema, ContactBookSchemaUpdateSchema
+from src.repository.contacts import get_contacts, get_contacts_birthday, get_contact_by_id, create_contact, update_contact, delete_contact
+from sqlalchemy.engine import Result
+
+
+class TestContactsRepository(unittest.IsolatedAsyncioTestCase):
+
+    def setUp(self):
+        self.user = User(id=1, username="testuser", password="testpass", confirmed=True)
+        self.session = AsyncMock(spec=AsyncSession)
+                               
+    async def test_get_contacts_basic(self):
+        user = User(id=1)
+        contacts = [
+            ContactBook(id=1, name="Alice", secondname="Smith", email="alice@example.com", user_id=1),
+            ContactBook(id=2, name="Bob", secondname="Jones", email="bob@example.com", user_id=1)
+        ]
+
+        mock_scalars = MagicMock()
+        mock_scalars.all.return_value = contacts
+
+        mock_result = MagicMock(spec=Result)
+        mock_result.scalars.return_value = mock_scalars
+
+        mock_db = AsyncMock()
+        mock_db.execute.return_value = mock_result
+
+        result = await get_contacts(limit=10, offset=0, db=mock_db, user=user)
+        self.assertEqual(result, contacts)
+
+    async def test_get_contacts_with_filters(self):
+        user = User(id=1)
+        contacts = [
+            ContactBook(id=1, name="Alice", secondname="Smith", email="abracadabra@example.com", user_id=1)]
+        mock_scalars = MagicMock()
+        mock_scalars.all.return_value = contacts
+        mock_result = MagicMock(spec=Result)
+        mock_result.scalars.return_value = mock_scalars
+        mock_db = AsyncMock()
+        mock_db.execute.return_value = mock_result
+        result = await get_contacts(limit=10, offset=0, db=mock_db, user=user, name="Alice", secondname=None, email="abracadabra@example.com")
+        self.assertEqual(result, contacts)
+
+    async def test_create_dcontact(self):
+        body = ContactBookSchema(
+            name="Alice",
+            secondname="Smith",
+            phone="+1234567890",  
+            email="valid@example.com",  
+            born_day=date(2000, 4, 30), 
+            additional_data=None
+        )
+        result = await create_contact(body, self.session, self.user)
+        self.assertIsInstance(result, ContactBook)
+
+    async def test_update_contact(self):
+        body = ContactBookSchemaUpdateSchema(
+            name="Alex",
+            secondname="Smith",
+            phone="+1234567890",  
+            email="valid@example.com",  
+            born_day=date(1999, 4, 7), 
+            additional_data=None
+        )
+        mocked_contact = MagicMock()
+        mocked_contact.scalar_one_or_none.return_value = ContactBook(id=1, name="Alice", secondname="Smith", email="abracadabra@example.com", user_id=1)
+        self.session.execute.return_value = mocked_contact
+        result = await update_contact(1, body, self.session, self.user.id)
+        self.assertIsInstance(result, ContactBook)
+        self.assertEqual(result.name, body.name)
+        self.assertEqual(result.secondname, body.secondname)
+        self.assertEqual(result.phone, body.phone)
+        self.assertEqual(result.email, body.email)
+        self.assertEqual(result.born_day, body.born_day)
+
+    async def test_delete_contact(self):
+        mocked_contact = MagicMock()
+        mocked_contact.scalar_one_or_none.return_value = ContactBook(id=1, name="Alice", secondname="Smith", email="abracadabra@example.com", user_id=self.user.id)
+        self.session.execute.return_value = mocked_contact
+        result = await delete_contact(1, self.session, self.user.id)
+        self.session.delete.assert_called_once()
+        self.session.commit.assert_called_once()
+
+        self.assertIsInstance(result, ContactBook)
+
+    async def test_get_contacts_birthday_None(self):
+        mocked_contact = MagicMock()
+        mocked_contact.scalar_one_or_none.return_value = ContactBook(id=1, name="Alex",
+            secondname="Smith",
+            phone="+1234567890",  
+            email="valid@example.com",  
+            born_day=date(1999, 4, 7), 
+            additional_data=None,
+            user_id=self.user.id)
+        self.session.execute.return_value = mocked_contact
+        result = await get_contacts_birthday(self.session, self.user.id)
+        self.assertEqual(len(result), 0)
+        
+
+    async def test_get_contacts_birthday(self):
+
+        test_contact = ContactBook(
+            id=1,
+            name="Alex",
+            secondname="Smith",
+            phone="+1234567890",
+            email="valid@example.com",
+            born_day=date(1999, 6, 21),  
+            additional_data=None,
+            user_id=self.user.id
+        )
+        
+
+        mock_scalars = MagicMock()
+        mock_scalars.all.return_value = [test_contact]  
+        
+        mock_result = MagicMock()
+        mock_result.scalars.return_value = mock_scalars
+        
+        self.session.execute.return_value = mock_result
+    
+        result = await get_contacts_birthday(self.session, self.user.id)
+        
+     
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].name, "Alex")
+        self.assertEqual(result[0].secondname, "Smith")
+        self.assertEqual(result[0].phone, "+1234567890")
+
+    
